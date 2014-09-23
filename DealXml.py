254de0dddf9921d6993fe22078b4387dbf4f7fc8
= <nianqinianyi@163.com>
2014-09-23 17:52:38 +0800

    再加一点东西

--- DealXml.py
+++ DealXml.py
@@ -1,13 +1,27 @@
 from xml.etree import ElementTree as et
+from SoftVo import Soft
 
 class DealXML():
     def loadXMLFile(self):
-        return et.parse(self.loadXMLFile())
+        return et.parse('conf/list.xml').getroot()
         
     def readXML(self):
         root = self.loadXMLFile()
-        
-        print("read")
+        softs = root.find("tab").findall("soft")
+        softList = []
+        for soft in softs:
+            vo = Soft()
+            vo.name = soft.get("name")
+            vo.foldername = soft.get("foldername")
+            vo.filename = soft.get("filename")
+            vo.row = soft.get("row")
+            vo.column = soft.get("column")
+            vo.cmd = soft.get("cmd")
+            paramList=soft.getchildren()
+            for param in paramList:
+                vo.param.append(param.text)
+            softList.append(vo)
+        return softList
     
     def addTab(self):
         root = self.loadXMLFile()
@@ -24,6 +38,15 @@ class DealXML():
     def editSoft(self):
         root = self.loadXMLFile()
         print("edit soft")
+    
+    def delTab(self):
+        root = self.loadXMLFile()
+        print("del tab")
+    
+    def delSoft(self):
+        root = self.loadXMLFile()
+        print("del soft")
 
 a=DealXML()
 a.loadXMLFile()
+print(a.readXML())
---
+++ SoftVo.py
@@ -0,0 +1,8 @@
+class Soft:
+    name=""
+    foldername=""
+    filename=""
+    row = ""
+    column = ""
+    cmd=""
+    param=[]
