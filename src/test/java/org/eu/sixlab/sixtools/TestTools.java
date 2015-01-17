538ac1712f477ed287b923c8b8c6f0412cd6f0c2
六楼的雨 <nianqinianyi@163.com>
2015-01-17 18:40:54 +0800

    换电脑

---
+++ src/test/java/org/eu/sixlab/sixtools/TestTools.java
@@ -0,0 +1,19 @@
+/* 
+ **************************************************************************************
+ * Copyright www.ebidding.net.cn 2014/12/25 Authors: 曹林伟 <caolinwei@ebidding.com.cn>*
+ **************************************************************************************
+ */
+package org.eu.sixlab.sixtools;
+
+/**
+ * 作者：曹林伟
+ * 创建时间：2014/12/25
+ * 功能描述：*****
+ * 版本：2.0.0
+ */
+public class TestTools {
+
+    public static void main(String[] args){
+        
+    }
+}
